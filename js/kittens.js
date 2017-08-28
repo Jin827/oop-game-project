5a6907d0d6dfa620f2b5fbf9b14d9a3b589f7f9a
Jin827 <hyojin.lee827@gmail.com>
2017-08-27 22:34:10 -0400

    Completed Cat game
    Team project / DecodeMTL

--- js/kittens.js
+++ js/kittens.js
@@ -2,16 +2,19 @@
 var GAME_WIDTH = 375;
 var GAME_HEIGHT = 500;
 
+// blah blah
+
 var ENEMY_WIDTH = 75;
 var ENEMY_HEIGHT = 156;
-var MAX_ENEMIES = 3;
+var MAX_ENEMIES = 3; // ENNEMIES !!!
 
 var PLAYER_WIDTH = 75;
 var PLAYER_HEIGHT = 54;
 
 // These two constants keep us from using "magic numbers" in our code
 var LEFT_ARROW_CODE = 37;
-var RIGHT_ARROW_CODE = 39;
+var RIGHT_ARROW_CODE = 40;
+var SHOOT_ARROW_CODE = 38;
 
 // These two constants allow us to DRY
 var MOVE_LEFT = 'left';
@@ -19,19 +22,43 @@ var MOVE_RIGHT = 'right';
 
 // Preload game images
 var images = {};
-['enemy.png', 'stars.png', 'player.png'].forEach(imgName => {
+['enemy.png', 'stars.png', 'player.png', 'kitten.png'].forEach(imgName => {
     var img = document.createElement('img');
     img.src = 'images/' + imgName;
     images[imgName] = img;
 });
 
+// SOUNDS
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }
+}
 
 
+
+class Entity {
+
+    render(ctx) {
+        ctx.drawImage(this.sprite, this.x, this.y);
+    }
+}
 
 
 // This section is where you will be doing most of your coding
-class Enemy {
+class Enemy extends Entity {
     constructor(xPos) {
+        super();
         this.x = xPos;
         this.y = -ENEMY_HEIGHT;
         this.sprite = images['enemy.png'];
@@ -43,19 +70,16 @@ class Enemy {
     update(timeDiff) {
         this.y = this.y + timeDiff * this.speed;
     }
-
-    render(ctx) {
-        ctx.drawImage(this.sprite, this.x, this.y);
-    }
 }
 
-class Player {
+class Player extends Entity {
     constructor() {
+        super();
         this.x = 2 * PLAYER_WIDTH;
         this.y = GAME_HEIGHT - PLAYER_HEIGHT - 10;
         this.sprite = images['player.png'];
-    }
 
+    }
     // This method is called by the game engine when left/right arrows are pressed
     move(direction) {
         if (direction === MOVE_LEFT && this.x > 0) {
@@ -66,11 +90,28 @@ class Player {
         }
     }
 
-    render(ctx) {
-        ctx.drawImage(this.sprite, this.x, this.y);
-    }
 }
 
+class Bullet extends Entity {
+    constructor(xPos) {
+        super();
+        this.sprite = images['player.png'];
+        this.boom = false;
+
+        // Each enemy should have a different speed
+        this.speed = 0.2;
+    }
+
+    shoot(playerPos){
+      this.x = playerPos;
+      this.y = GAME_HEIGHT - (PLAYER_HEIGHT*2) ;
+    }
+
+    update(timeDiff) {
+        this.y = this.y - timeDiff * this.speed;
+    }
+
+}
 
 
 
@@ -80,11 +121,13 @@ This section is a tiny game engine.
 This engine will use your Enemy and Player classes to create the behavior of the game.
 The engine will try to draw your game at 60 frames per second using the requestAnimationFrame function
 */
+
 class Engine {
     constructor(element) {
         // Setup the player
         this.player = new Player();
-
+        this.bullet = new Bullet();
+        var mySound = new sound("music/shot.mp3");
         // Setup enemies, making sure there are always three
         this.setupEnemies();
 
@@ -114,15 +157,21 @@ class Engine {
         }
     }
 
+
     // This method finds a random spot where there is no enemy, and puts one in there
     addEnemy() {
-        var enemySpots = GAME_WIDTH / ENEMY_WIDTH;
+        var enemySpots = (GAME_WIDTH / ENEMY_WIDTH);
 
-        var enemySpot;
+        var enemySpot; //UNDEFINED
+
         // Keep looping until we find a free enemy spot at random
-        while (!enemySpot || this.enemies[enemySpot]) {
+        while (enemySpot === undefined || this.enemies[enemySpot]) {
             enemySpot = Math.floor(Math.random() * enemySpots);
         }
+
+        //1ST STEP
+        // enemySpot === undefined, this.enemies[enemySpot] === UNDEFINED
+        // enemySpot ===1
 
         this.enemies[enemySpot] = new Enemy(enemySpot * ENEMY_WIDTH);
     }
@@ -139,6 +188,8 @@ class Engine {
             }
             else if (e.keyCode === RIGHT_ARROW_CODE) {
                 this.player.move(MOVE_RIGHT);
+            } else if (e.keyCode === SHOOT_ARROW_CODE) {
+                this.bullet.shoot(this.player.x);
             }
         });
 
@@ -155,21 +206,32 @@ class Engine {
     To account for the fact that we don't always have 60 frames per second, gameLoop will send a time delta argument to `update`
     You should use this parameter to scale your update appropriately
      */
+
     gameLoop() {
         // Check how long it's been since last frame
         var currentFrame = Date.now();
         var timeDiff = currentFrame - this.lastFrame;
+        //console.log(mySound);
 
         // Increase the score!
         this.score += timeDiff;
 
         // Call update on all enemies
         this.enemies.forEach(enemy => enemy.update(timeDiff));
+        // Call update on all the bullet
+        this.bullet.update(timeDiff);
+
 
         // Draw everything!
         this.ctx.drawImage(images['stars.png'], 0, 0); // draw the star bg
         this.enemies.forEach(enemy => enemy.render(this.ctx)); // draw the enemies
         this.player.render(this.ctx); // draw the player
+        this.bullet.render(this.ctx); // draw the player
+
+
+        this.killCat();
+
+        //
 
         // Check if any enemies should die
         this.enemies.forEach((enemy, enemyIdx) => {
@@ -177,7 +239,10 @@ class Engine {
                 delete this.enemies[enemyIdx];
             }
         });
+
         this.setupEnemies();
+
+
 
         // Check if player is dead
         if (this.isPlayerDead()) {
@@ -185,6 +250,22 @@ class Engine {
             this.ctx.font = 'bold 30px Impact';
             this.ctx.fillStyle = '#ffffff';
             this.ctx.fillText(this.score + ' GAME OVER', 5, 30);
+            this.ctx.fillText('START AGAIN?', 100, 200);
+            this.ctx.fillText('PRESS RESTART', 95, 280);
+            console.log("dead");
+            // create button
+            // var button = {}; //initialized
+            //
+            var button = document.createElement("button");
+            var theNode = document.createTextNode("RESTART");
+            var body = document.getElementsByTagName('body')[0];
+            button.appendChild(theNode);
+            body.appendChild(button);
+            button.addEventListener('click', () => {
+              this.score = 0;
+              this.gameLoop();
+            })
+
         }
         else {
             // If player is not dead, then draw the score
@@ -198,11 +279,46 @@ class Engine {
         }
     }
 
+    killCat() {
+      for (var i=0; i<5; i++){
+        if (this.enemies[i] != undefined &&
+        this.enemies[i].x == this.bullet.x &&
+      this.enemies[i].y > (this.bullet.y - 100))
+        { delete this.enemies[i];
+          console.log(this.mySound); // prints undefined
+          mySound.play(); // UncoughtRefError : MySound is not defined
+          // it doesn't work neither when I use this.mySound.play() ...
+        }
+      }
+    }
+
+
     isPlayerDead() {
         // TODO: fix this function!
-        return false;
+        // this.y = GAME_HEIGHT - PLAYER_HEIGHT - 10;
+        // this.x = 2 * PLAYER_WIDTH;
+        var dead = false;
+        // var heightLIM = GAME_HEIGHT - (PLAYER_HEIGHT/2) - 10 +(ENEMY_HEIGHT/2);
+
+        for(var i=0; i<5; i++){
+          if (this.enemies[i] != undefined) {
+            if (this.enemies[i].x == this.player.x
+              && this.enemies[i].y >= GAME_HEIGHT - PLAYER_HEIGHT - 50 &&
+            this.enemies[i].y <= GAME_HEIGHT - (PLAYER_HEIGHT/2)){
+              dead = true;
+            }
+          }
+        }
+        //var ENEMY_WIDTH = 75;
+        //var ENEMY_HEIGHT = 156;
+        //var MAX_ENEMIES = 3; // ENNEMIES !!!
+        return dead;
     }
 }
+
+
+
+
 
 
 
